746230668e9b8dfcd0c8f7e321efde3621253a11
sudo-gotcha <136983179+sudo-gotcha@users.noreply.github.com>
2023-06-19 00:21:34 +0900

    Create ScannerConsole.java

---
+++ dir1/ScannerConsole.java
@@ -0,0 +1,22 @@
+// package test
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class RunManager {
+
+	public static void main(String[] args) {
+    //기본형		
+    
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNextLine()) {
+						
+			String[] consoleIn = sc.nextLine()										
+			System.out.println(result);
+								
+		}
+		sc.close();
+	}
+
+}
